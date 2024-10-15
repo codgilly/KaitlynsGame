4cea4a681af228b1435d2c95d5b72cc9664d2443
Cody gilbert <146819086+codgilly@users.noreply.github.com>
2024-10-15 07:48:30 +0100

    fixed and buggy

---
+++ Assets/TestGame/Scripts/Player/TestCamerScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCamerScript : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject playerObject;
+
+    public float playerPos;
+    public float cameraPosX;
+
+    public Vector3 position;
+    void Start()
+    {
+        cameraPosX = camera.transform.position.z;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerPos = playerObject.transform.position.z;
+
+        position = new Vector3(0, 3, playerPos - 5);
+
+
+        // Vector3 cameraPosition = new Vector3(playerPos, position);
+
+        camera.transform.localPosition = position;
+    }
+
+    //https://www.youtube.com/watch?v=HMuhqNo5PZ8&t=209s video for script, i missed some stuff for the inspector i think
+}
--- Assets/TestGame/Scripts/Player/TestPlayer3D.cs
+++ Assets/TestGame/Scripts/Player/TestPlayer3D.cs
@@ -4,69 +4,48 @@ using UnityEngine;
 
 public class TestPlayer3D : MonoBehaviour
 {
-    //this is a online script I have found, in the unity project you can ajust these values to allow the player to move how you would like them to
+    public float speed;
+    public float jumpForce;
 
-    public Camera playerCamera; //this is how the player moves based off, so how you move the camera this is how the player moves
-    public float walkSpeed = 6f;
-    public float runSpeed = 12f;
-    public float jumpPower = 7f;
-    public float gravity = 10f;
-    public float lookSpeed = 2f;
-    public float lookXLimit = 45f;
-    public float defaultHeight = 2f;
-    public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
+    [Space(15)]
+    public float CheckDistance;
+    public Transform GroundCheck;
+    public LayerMask GroundMask;
 
-    private Vector3 moveDirection = Vector3.zero;
-    private float rotationX = 0;
-    private CharacterController characterController;
+    [Space(15)]
+    public Transform PlayerMesh;
 
-    public GameObject camLookAt;
+    [Space(15)]
+    public bool canJump;
+    public bool canMove;
 
-    private bool canMove = true;
-
-    void Start()
+    void FixedUpdate()
     {
-        characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
 
-    void Update()
-    {
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
+        Rigidbody rb = GetComponent<Rigidbody>();
 
-       
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 MoveDirection = forward * verticalInput + right * horizontalInput;
+
+        rb.velocity = new Vector3(MoveDirection.x * speed, rb.velocity.y, MoveDirection.z * speed);
+
+        if (MoveDirection != new Vector3(0, 0, 0))
         {
-            moveDirection.y = jumpPower;
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY;
-        }
+            PlayerMesh.rotation = Quaternion.LookRotation(MoveDirection);
 
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
-
-        characterController.Move(moveDirection * Time.deltaTime);
-
-        if (canMove)
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
 }
+
